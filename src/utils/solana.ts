4363f2716819a66708de213fde1492bdfb0e4338
blockiosaurus <blockiosaurus@gmail.com>
2023-07-29 21:15:56 -0400

    Furthing phantom support.

--- src/utils/solana.ts
+++ src/utils/solana.ts
@@ -5,25 +5,27 @@ export function getWalletAddress() {
     if (isXNFT()) {
         address = window.xnft.solana.publicKey;
     } else {
-        console.log(window.phantom.solana.isConnected);
-        console.log(window.phantom.solana.isPhantom);
-        console.log(window.phantom.solana.publicKey);
+        // console.log(window.phantom.solana.isConnected);
+        // console.log(window.phantom.solana.isPhantom);
+        // console.log(window.phantom.solana.publicKey);
         address = window.phantom.solana.publicKey;
     }
-    console.log(address);
+    // console.log(address);
     return address;
 }
 
 export function getConnection() {
+    if (isXNFT()) {
     return new Connection(window.xnft.solana.connection.rpcEndpoint, { confirmTransactionInitialTimeout: 600000 });
+    } else {
+        return new Connection("https://solana-mainnet.rpc.extrnode.com", { confirmTransactionInitialTimeout: 600000 });
+    }
 }
 
 export function isXNFT() {
     if (window.xnft === undefined || window.xnft.solana === undefined || window.xnft.solana.isXnft === undefined) {
-        console.log("Not XNFT");
         return false;
     } else {
-        console.log("Is XNFT");
         return true;
     }
 }
